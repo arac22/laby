882fce4da3d3ad559f69184fd7628c0bb1e771e7
arac22 <rocchi.alb@gmail.com>
2014-12-04 00:58:57 +0100

    Added {{#each this}} to iterate over array

--- client/play.js
+++ client/play.js
@@ -5,13 +5,12 @@ Template.play.events({
       // this is a card
       var gameId = template.data._id;
       var game = Games.findOne({_id: gameId});
-      console.log('game: '+game);
-      console.log('this' + this);
+      // console.log('game: '+game);
+      // console.log('this' + this);
       game.board[this.index].rotation = rotateCW( game.board[this.index].rotation );
       Games.update(gameId,game);
  	}		
 });
-
 
 function rotateCW(rotation){
   rotation++;
